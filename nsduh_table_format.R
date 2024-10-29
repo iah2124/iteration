dc84f989b32ca01ee7f23ee4db3f4d57094449eb
Iris Hart <iah2124@cumc.columbia.edu>
2024-10-29 11:20:57 -0400

    commit final

--- nsduh_table_format.R
+++ nsduh_table_format.R
@@ -1,5 +1,6 @@
-```{r}
+
 nsduh_table_format =function(html, table_num, table_name) {
+  
   out_table = 
     html |>
     html_table() |>
@@ -7,12 +8,7 @@ nsduh_table_format =function(html, table_num, table_name) {
     slice(-1) |> 
     mutate(drug = table_name) |>
     select(-contains("P Value"))
+  
   return(out_table)
 }
 
-bind_rows(
-  nsduh_table_format(html = nsduh_html, 1, "marj"),
-  nsduh_table_format(html = nsduh_html, 4, "cocaine"),
-  nsduh_table_format(html = nsduh_html, 5, "heroin")
-)
-```
